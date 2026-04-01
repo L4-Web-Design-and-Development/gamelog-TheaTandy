a3e8360e2f32a6cfb26545e6d46d0aa699185c21
TheaTandy <thea.tandy@outlook.com>
2026-04-01 16:28:16 +0100

    Updated game logger web page
    Web page now shows game titles and prices.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,3 +1,6 @@
+import { PrismaClient } from '@prisma/client'
+import { json } from '@remix-run/node'
+import { useLoaderData } from '@remix-run/react'
 import type { MetaFunction } from "@remix-run/node";
 
 export const meta: MetaFunction = () => {
@@ -7,10 +10,28 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export async function loader() {
+  const prisma = new PrismaClient()
+
+  const games = await prisma.game.findMany()
+
+  return json({ games })
+}
 export default function Index() {
+    const { games } = useLoaderData<typeof loader>()
+
+
   return (
     <div className="flex items-center justify-center min-h-screen">
+      <div>
       <h1 className="text-4xl font-bold">Hello, GameLogger!</h1>
+     {games.map((game) => (
+          <div key={game.id}>
+            <h2>{game.title}</h2>
+            <h2>£{game.price.toFixed(2)}</h2>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
